3b848cbe60d2e5c9253715abdd317913472580e8
NinhLe <lephuocninh7118@gmail.com>
2020-01-02 11:40:09 +0700

    Set length for  image field all articles

---
+++ database/1-2-2020_update_image_field.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `blog_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `book_group_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `election_central_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `environment_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `film_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `must_reads_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `new_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `opinion_debate_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `queries_articles` CHANGE `featured_image` `featured_image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
